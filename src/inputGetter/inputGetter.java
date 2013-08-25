33df2298d208367e92f04c171be91c7ab7e278d1
endlessfrank <endlessfrank@qq.com>
2013-08-25 16:43:06 +0800

    Full Version

--- src/inputGetter/inputGetter.java
+++ src/inputGetter/inputGetter.java
@@ -26,10 +26,10 @@ public class inputGetter implements performable{
 		   Scanner in = new Scanner(System.in);
 		   Queue<String> titles = new LinkedList<String>();
 		   System.out.println("Please enter titles, press ! to end.");
-		   String s = in.nextLine();
-		   while(s != "!"){
+		   String s="";
+		   
+		   while(in.hasNextLine() && (s=in.nextLine()).length()>0){
 			   titles.offer(s);
-			   s = in.nextLine();
 		   }
 		   in.close();
 		   return titles;
@@ -37,8 +37,8 @@ public class inputGetter implements performable{
 
 	@Override
 	public boolean performTask() {
-		Queue<String>titles = getTitles();
 		Queue<String>wordsToIgnore = getWordsToIgnore();
+		Queue<String>titles = getTitles();
 		store.titles = titles;
 		store.wordsToIgnore = wordsToIgnore;
 		// TODO Auto-generated method stub
