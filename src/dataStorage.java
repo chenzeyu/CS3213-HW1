774fe6cad0ddf65b0c282190766c05dc2e52b415
Chen Zeyu <zeyufly@gmail.com>
2013-08-25 15:18:38 +0800

    add interface

---
+++ src/dataStorage.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class dataStorage {
+	public Queue<String> wordsToIgnore;
+	public Queue<String> titles;
+	public Queue<String> results;
+}
---
+++ src/performable.java
@@ -0,0 +1,4 @@
+
+public interface performable {
+	public void performTask();
+}
